08b6725a388ab04164a61d21659fb1ed29010139
Satyabhama-Reddy <satyabhamareddy@gmail.com>
2024-04-06 11:35:32 -0500

    satya

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -232,7 +232,7 @@ const GlobalStyle = createGlobalStyle`
   .subtitle {
     color: ${colors.green};
     margin: 0 0 20px 0;
-    font-size: ${fontSizes.md};
+    font-size: ${fontSizes.sm};
     font-family: ${fonts.SFMono};
     font-weight: normal;
     line-height: 1.5;
